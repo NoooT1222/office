d51d563ece74e30f28151b96d0bab9e61b0f39d8
shingetsu-mgmt-proj <61572766+shingetsu-mgmt-proj@users.noreply.github.com>
2020-08-07 23:51:49 +0900

    2020/08/07

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ $(function(){
   $(document).ready( function(){
   // ページ読み込み時に実行したい処理
 
-    $(".logo").fadeIn(2000);
+    $(".index_logo_img").slideDown(300);
+    $(".display_none").slideDown(1000);
 
     setTimeout(function(){
-      $(".loading").animate({
-        'width':'50vw'
-      },1000);
+
+
     },2000);
 
     setTimeout(function(){
@@ -38,6 +38,17 @@ $(function(){
     }
   );
 
+  $(".index_menu_contents a").hover(
+    function(){
+      $(this).css('background-color','#f8b500');
+
+    },
+    function(){
+      $(this).css('background-color','#30B9F0');
+
+    }
+  );
+
   $(".news_column").click(function(){
   var $answer = $(this).find(".news_content");
   if($answer.hasClass("news_open")){
@@ -53,37 +64,13 @@ $(function(){
 
   $(".menu_link").hover(
     function(){
-      $(this).css('color','#5d8729');
+      $(this).css('color','#f8b500');
     },
     function(){
-      $(this).css('color','white');
+      $(this).css('color','black');
     }
   );
 
-  $(".menu_link").click(
-    function(){
-      $(".about_overview").show();
-      $(".about_me").show();
-      $(".about_history").show();
-      var id = $(this).attr('id');
-      switch(id){
-        case "history_btn":
-          $(".about_overview").fadeOut();
-          $(".about_me").fadeOut();
-          break;
-
-        case "overview_btn":
-          $(".about_history").fadeOut();
-          $(".about_me").fadeOut();
-          break;
-
-        case "aboyt_us":
-          $(".about_history").fadeOut();
-          $(".about_overview").fadeOut();
-          break;
-      };
-    }
-  );
 
   $(".example").click(function(){
   var $answer = $(this).find(".example_answer");
